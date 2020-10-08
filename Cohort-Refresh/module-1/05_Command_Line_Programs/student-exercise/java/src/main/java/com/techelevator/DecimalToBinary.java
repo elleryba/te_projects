708366aaf8b9c6fb39ca5b668f767c3ece3e2cd2
ERYBA <elleryba@icloud.com>
2020-10-08 10:50:59 -0400

    DecimalToBinary completed

--- Cohort-Refresh/module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ Cohort-Refresh/module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,27 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
 
+		System.out.println("Please enter a series of decimal values, separated by spaces: ");
+		String userResponse = userInput.nextLine();
+
+		try {
+			String[] splitString = userResponse.split(" ");
+
+			for (String s : splitString) {
+				System.out.println(s + " in binary is " + Integer.toBinaryString(Integer.parseInt(s)));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Invalid entry! Try again.");
+			System.exit(0);
+		} finally {
+			userInput.close();
+		}
 	}
-
 }
