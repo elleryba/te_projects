082049c43784c5a9589bca84b7024cdd7f82c0c5
ERYBA <elleryba@icloud.com>
2020-10-08 09:44:01 -0400

    Fibonacci while loop is not executing properly

--- Cohort-Refresh/module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ Cohort-Refresh/module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		int sum = 0;
+		int x1 = 0;
+		int x2 = 1;
+		int userNum = 0;
+		Scanner userInput = new Scanner(System.in);
 
+		System.out.print("Please enter the Fibonacci number: ");
+		String userResponse = userInput.nextLine();
+
+		try {
+			userNum = Integer.parseInt(userResponse);
+			
+			while (sum < userNum) {
+				for (int i = 0; i < userNum; i++) {
+					System.out.println(x1);
+					sum = x1 + x2;
+					x1 = x2;
+					x2 = sum;
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Invalid input! Try again.");
+			System.exit(0);
+		} finally {
+			userInput.close();
+		}
 	}
-
 }
