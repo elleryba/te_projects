1786a6491ad504ab157249e0d84dccf5287dde3c
ERYBA <elleryba@icloud.com>
2020-10-08 09:08:18 -0400

    TempConvert completed

--- Cohort-Refresh/module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ Cohort-Refresh/module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,44 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		double convertedTemp = 0;
+		double temp = 0;
 
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter a numeric temperature to convert: ");
+
+		String userTemp = userInput.nextLine();
+
+			try {
+				temp = Double.parseDouble(userTemp);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.out.println("Invalid input! Try again.");
+				System.exit(0);
+			}
+
+		System.out.println("Is this (C)elsius or (F)ahrenheit? ");
+		String userResponse = userInput.nextLine().toLowerCase();
+
+		try {
+			if (userResponse.isEmpty()) {
+				System.out.println("Is this (C)elsius or (F)ahrenheit? ");
+			} else if (userResponse.equals("f")) {
+				convertedTemp = (temp - 32) / 1.8;
+				System.out.println(temp + " degrees Fahrenheit is equal to " + convertedTemp + " degrees Celsius.");
+			} else if (userResponse.equals("c")) {
+				convertedTemp = temp * 1.8 + 32;
+				System.out.println(temp + " degrees Celsius is equal to " + convertedTemp + " degrees Fahrenheit.");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			userInput.close();
+		}
 	}
 
 }
